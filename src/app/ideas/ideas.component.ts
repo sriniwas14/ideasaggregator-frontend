12b8861773d7e248813f3893a47e489eb4a87af8
sriniwas <sriniwasx@gmail.com>
2019-11-23 21:45:55 +0530

    Created and designed ideas component

--- src/app/ideas/ideas.component.ts
+++ src/app/ideas/ideas.component.ts
@@ -6,6 +6,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ideas.component.scss']
 })
 export class IdeasComponent implements OnInit {
+  items : Object = [
+      {
+        title: "Wohoo",
+        description: "Wohoo some more",
+        color: "red"
+      },
+      {
+        title: "Niks",
+        description: "Niks some more",
+        color: "blue"
+      },
+      {
+        title: "Iota",
+        description: "Iota some more",
+        color: "green"
+      },
+      {
+        title: "Niks",
+        description: "Niks some more",
+        color: "blue"
+      },
+      {
+        title: "Iota",
+        description: "Iota some more",
+        color: "green"
+      },
+      {
+        title: "Niks",
+        description: "Niks some more",
+        color: "blue"
+      },
+      {
+        title: "Iota",
+        description: "Iota some more",
+        color: "green"
+      }
+  ]
 
   constructor() { }
 
