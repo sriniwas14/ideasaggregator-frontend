b344b58a3b171c4c54c8c02bd242ce32e1dfc1dd
sriniwas <sriniwasx@gmail.com>
2019-11-23 16:34:43 +0530

    Created Header Component

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor(private http: HttpService) { }
+
+  ngOnInit() {
+  }
+
+  logout() {
+    this.http.logout();
+  }
+
+}
